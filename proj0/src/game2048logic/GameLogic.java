fc0a6438a9498b323f209cd2513d330301bc25ca
Alex24nest <anesterov2404@gmail.com>
2025-07-21 00:13:27 -0400

    Tasks 1-6: Complited

---
+++ proj0/src/game2048logic/GameLogic.java
@@ -0,0 +1,95 @@
+package game2048logic;
+
+import game2048rendering.Side;
+import static game2048logic.MatrixUtils.rotateLeft;
+import static game2048logic.MatrixUtils.rotateRight;
+
+/**
+ * @author  Josh Hug
+ */
+public class GameLogic {
+    /** Moves the given tile up as far as possible, subject to the minR constraint.
+     *
+     * @param board the current state of the board
+     * @param r     the row number of the tile to move up
+     * @param c -   the column number of the tile to move up
+     * @param minR  the minimum row number that the tile can land in, e.g.
+     *              if minR is 2, the moving tile should move no higher than row 2.
+     * @return      if there is a merge, returns the 1 + the row number where the merge occurred.
+     *              if no merge occurs, then return 0.
+     */
+    public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
+        // TODO: Fill this in in tasks 2, 3, 4
+        int row = r;
+        for (int i = 1; i <= r-minR; i++){
+            if (board[r-i][c] == 0) {
+                row = r-i;
+            }
+        }
+
+        if (row != r) {
+            board[row][c] = board[r][c];
+            board[r][c] = 0;
+        }
+        if (row != 0) {
+            if (board[row][c] == board[row - 1][c] && minR <= row - minR) {
+                board[row - 1][c] *= 2;
+                board[row][c] = 0;
+                return row;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Modifies the board to simulate the process of tilting column c
+     * upwards.
+     *
+     * @param board     the current state of the board
+     * @param c         the column to tilt up.
+     */
+    public static void tiltColumn(int[][] board, int c) {
+        // TODO: fill this in in task 5
+        int minR = 0;
+        for (int i = 1; i <= 3; i++) {
+            if (board[i][c] != 0) {
+                minR = moveTileUpAsFarAsPossible(board, i, c, minR);
+            }
+        }
+        return;
+    }
+
+    /**
+     * Modifies the board to simulate tilting all columns upwards.
+     *
+     * @param board     the current state of the board.
+     */
+    public static void tiltUp(int[][] board) {
+        // TODO: fill this in in task 6
+        for (int i = 0; i <= 3; i++) {
+            tiltColumn(board, i);
+        }
+        return;
+    }
+
+    /**
+     * Modifies the board to simulate tilting the entire board to
+     * the given side.
+     *
+     * @param board the current state of the board
+     * @param side  the direction to tilt
+     */
+    public static void tilt(int[][] board, Side side) {
+        // TODO: fill this in in task 7
+        if (side == Side.EAST) {
+            return;
+        } else if (side == Side.WEST) {
+            return;
+        } else if (side == Side.SOUTH) {
+            return;
+        } else {
+            tiltUp(board);
+            return;
+        }
+    }
+}
